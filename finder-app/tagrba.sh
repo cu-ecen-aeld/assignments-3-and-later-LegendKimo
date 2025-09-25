a1461538aa916ab9367f8a3ab94d75915d11a879
kareem khaled <k.kh1994@yahoo.com>
2025-09-25 19:09:31 +0300

    fix make all issue for assignment4

---
+++ finder-app/tagrba.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Script outline to install and build kernel.
+# Author: kkh
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+cd .. 
+ls -al 
+echo " the test begins "
+ls -al "${SCRIPT_DIR}/"
